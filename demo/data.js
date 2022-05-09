7d4e9539c587a556d4ce575200bea97714a4be47
Lucas Oliveira <99743815+Lykked@users.noreply.github.com>
2022-05-09 15:46:52 -0300

    Add files via upload
    heatmap diferenciado

---
+++ demo/data.js
@@ -0,0 +1,20 @@
+var data2 = [];
+
+for (var i = 0; i < 900; ++i) {
+    data2.push([Math.random() * 100 + i / 1.5 + 310, 
+        Math.random() * 10 - i / 4 + 300, Math.random()]);
+}
+
+var data3   = [];
+
+for (var i = 0; i < 120; ++i) {
+    data2.push([Math.random() * 80 + i / 1.5 + 260, 
+        Math.random() * 9 - i / 2 + 200, Math.random()]);
+}
+
+var data4   = [];
+
+for (var i = 0; i < 120; ++i) {
+    data2.push([Math.random() * 80 + i / 1.5 + 410, 
+        Math.random() * 9 - i / 2 + 450, Math.random()]);
+}
